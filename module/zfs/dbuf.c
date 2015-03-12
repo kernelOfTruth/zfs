d6b36ff01ddeac5d6290696e053d2312b956d38e
Brian Behlendorf <behlendorf1@llnl.gov>
2015-03-13 00:32:41 +0100

    Restore dbuf_write_ready() early return #3158
    Commit b0bc7a8 reworked the dbuf_write_ready() function to remove the
    early return when a hole is detected. This does simplify the code but
    it means additional contention of the db->db_mtx lock which can impact
    performance.
    
    Specificially the tiotest benchmark shows a significant performance
    regression because it reads/writes zero-filled buffers which are converted
    to holes. Without the early return optimization the additional lock
    contention on the dbufs is significant. Cached read performance for
    holes is cut in half. The performance of non-hole block pointers
    remains roughly the same as expected.
    
    Signed-off-by: Brian Behlendorf behlendorf1@llnl.gov
    Issue #3106

--- module/zfs/dbuf.c
+++ module/zfs/dbuf.c
@@ -2698,6 +2698,12 @@ dbuf_write_ready(zio_t *zio, arc_buf_t *buf, void *vdb)
 	dnode_diduse_space(dn, delta - zio->io_prev_space_delta);
 	zio->io_prev_space_delta = delta;
 
+	if (BP_IS_HOLE(bp)) {
+		ASSERT0(bp->blk_fill);
+		DB_DNODE_EXIT(db);
+		return;
+	}
+
 	if (bp->blk_birth != 0) {
 		ASSERT((db->db_blkid != DMU_SPILL_BLKID &&
 		    BP_GET_TYPE(bp) == dn->dn_type) ||
@@ -2732,11 +2738,7 @@ dbuf_write_ready(zio_t *zio, arc_buf_t *buf, void *vdb)
 					fill++;
 			}
 		} else {
-			if (BP_IS_HOLE(bp)) {
-				fill = 0;
-			} else {
-				fill = 1;
-			}
+			fill = 1;
 		}
 	} else {
 		blkptr_t *ibp = ABD_TO_BUF(db->db.db_data);
